883ac29af8e9f41985419834ddae3fb4a8582b6a
demo_1 <vermatanuj905@gmail.com>
2023-09-03 14:47:43 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Enter no:-");
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      for(int i=0;i<n;i++){
+          System.out.println("i:- "+ i);
+          for(int j=0;j<Math.sqrt(n);j++){
+              System.out.println("hello:-"+ "i:-"+i+ "  j:-" + j);
+          }
+      }
+
+
+    }
+}
